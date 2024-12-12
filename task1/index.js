32a9d438a590e85250e4146f2b4a8984e75c9295
Yuriichulkov1 <yuriichulkov@college.udhtu.edu.ua>
2024-12-12 17:03:01 +0200

    Update index.js

--- task1/index.js
+++ task1/index.js
@@ -5,7 +5,6 @@ const calculateAreaExpression = function(width, height) {
 const areaExpression = calculateAreaExpression(5, 10);
 console.log(`Function Expression: ${areaExpression}`);
 
-// Функція як стрілкова функція (arrow function)
 const calculateAreaArrow = (width, height) => width * height;
 
 const areaArrow = calculateAreaArrow(5, 10);
